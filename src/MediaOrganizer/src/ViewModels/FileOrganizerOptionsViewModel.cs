14abb33abbb738f6ba91dcd1930503c4311c8f9c
mkArtak <artak.services+github@outlook.com>
2025-06-04 19:15:48 -0700

    - Added a new window for editing media categories - Implemented save / delete functionality

--- src/MediaOrganizer/src/ViewModels/FileOrganizerOptionsViewModel.cs
+++ src/MediaOrganizer/src/ViewModels/FileOrganizerOptionsViewModel.cs
@@ -1,64 +1,132 @@
 ﻿using MediaOrganizer.Core;
+using MediaOrganizer.Views;
+using Prism.Commands;
+using Prism.Dialogs;
 using Prism.Mvvm;
-using System.Collections.Generic;
+using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Windows;
 
-namespace MediaOrganizer.ViewModels
+namespace MediaOrganizer.ViewModels;
+
+public class FileOrganizerOptionsViewModel : BindableBase
 {
-    public class FileOrganizerOptionsViewModel : BindableBase
+    private string sourceRoot;
+    private string destinationRoot;
+    private bool removeSource;
+    private bool skipExistingFiles;
+    private string destinationPattern = "{Year}/{MonthName}/{Year}-{Month}-{Day}";
+    private bool deleteEmptyFolders;
+    private ObservableCollection<MediaCategory> mediaCategories;
+    private Window _currentDialog;
+
+    public bool SkipExistingFiles { get => skipExistingFiles; set => SetProperty(ref skipExistingFiles, value); }
+
+    public bool DeleteEmptyFolders { get => deleteEmptyFolders; set => SetProperty(ref deleteEmptyFolders, value); }
+
+    public bool RemoveSource { get => removeSource; set => SetProperty(ref this.removeSource, value); }
+
+    public string DestinationRoot { get => destinationRoot; set => SetProperty(ref this.destinationRoot, value); }
+
+    public string SourceRoot { get => sourceRoot; set => SetProperty(ref this.sourceRoot, value); }
+
+    public string DestinationPattern { get => destinationPattern; set => SetProperty(ref destinationPattern, value); }
+
+    public ObservableCollection<MediaCategory> MediaCategories { get => mediaCategories; set => SetProperty(ref mediaCategories, value); }
+
+    public DelegateCommand AddCategoryCommand { get; init; }
+
+    public DelegateCommand<MediaCategory> EditCategoryCommand { get; init; }
+
+    public FileOrganizerOptionsViewModel() : base()
     {
-        private string sourceRoot;
-        private string destinationRoot;
-        private bool removeSource;
-        private bool skipExistingFiles;
-        private string destinationPattern = "{Year}/{MonthName}/{Year}-{Month}-{Day}";
-        private bool deleteEmptyFolders;
-        private List<MediaCategory> mediaCategories;
+        AddCategoryCommand = new DelegateCommand(OnAddCategory);
+        EditCategoryCommand = new DelegateCommand<MediaCategory>(OnEditCategory);
+    }
 
-        public bool SkipExistingFiles { get => skipExistingFiles; set => SetProperty(ref skipExistingFiles, value); }
+    public FileOrganizerOptionsViewModel(FilesOrganizerOptions options) : this()
+    {
+        this.SourceRoot = options.SourceRoot;
+        this.DestinationRoot = options.DestinationRoot;
+        this.RemoveSource = options.RemoveSource;
+        this.SkipExistingFiles = options.SkipExistingFiles;
+        this.DestinationPattern = options.DestinationPattern;
+        this.DeleteEmptyFolders = options.DeleteEmptyFolders;
+        this.MediaCategories = new ObservableCollection<MediaCategory>(options.MediaCategories);
+    }
 
-        public bool DeleteEmptyFolders { get => deleteEmptyFolders; set => SetProperty(ref deleteEmptyFolders, value); }
-
-        public bool RemoveSource { get => removeSource; set => SetProperty(ref this.removeSource, value); }
-
-        public string DestinationRoot { get => destinationRoot; set => SetProperty(ref this.destinationRoot, value); }
-
-        public string SourceRoot { get => sourceRoot; set => SetProperty(ref this.sourceRoot, value); }
-
-        public string DestinationPattern { get => destinationPattern; set => SetProperty(ref destinationPattern, value); }
-
-        public List<MediaCategory> MediaCategories { get => mediaCategories; set => SetProperty(ref mediaCategories, value); }
-
-        public FileOrganizerOptionsViewModel() : base()
+    public FilesOrganizerOptions GetOptions()
+    {
+        var result = new FilesOrganizerOptions
         {
+            SourceRoot = this.SourceRoot,
+            DestinationRoot = this.DestinationRoot,
+            RemoveSource = this.RemoveSource,
+            SkipExistingFiles = this.SkipExistingFiles,
+            DestinationPattern = this.DestinationPattern,
+            DeleteEmptyFolders = this.DeleteEmptyFolders,
+        };
 
+        result.MediaCategories.AddRange(this.MediaCategories ?? new ObservableCollection<MediaCategory>());
+
+        return result;
+    }
+
+    private void OnAddCategory()
+    {
+        ShowCategoryEditorDialog(new MediaCategory());
+    }
+
+    private void OnEditCategory(MediaCategory category)
+    {
+        if (category != null)
+        {
+            ShowCategoryEditorDialog(category);
         }
+    }
 
-        public FileOrganizerOptionsViewModel(FilesOrganizerOptions options) : base()
-        {
-            this.SourceRoot = options.SourceRoot;
-            this.DestinationRoot = options.DestinationRoot;
-            this.RemoveSource = options.RemoveSource;
-            this.SkipExistingFiles = options.SkipExistingFiles;
-            this.DestinationPattern = options.DestinationPattern;
-            this.DeleteEmptyFolders = options.DeleteEmptyFolders;
-            this.MediaCategories = options.MediaCategories;
-        }
+    private void ShowCategoryEditorDialog(MediaCategory category)
+    {
+        var vm = new MediaCategoryEditorViewModel(category, ValidateCategory,
+            onSave: cat => { MediaCategories.Add(cat); _currentDialog.Close(); },
+            onDelete: cat => { MediaCategories.Remove(MediaCategories.Single(c => c.CategoryName == cat.CategoryName)); _currentDialog.Close(); }
+            );
 
-        public FilesOrganizerOptions GetOptions()
+        _currentDialog = new MediaCategoryEditorWindow
         {
-            var result = new FilesOrganizerOptions
+            DataContext = vm
+        };
+        _currentDialog.ShowDialog();
+        _currentDialog = null;
+    }
+
+    private bool ValidateCategory(MediaCategory category)
+    {
+        // The validation is done in two steps:
+        // 1. Check if another category with the same name already exists
+        // 2. Check if any other category has an extension defined by this category
+        if (string.IsNullOrWhiteSpace(category.CategoryName))
+            return false;
+
+        if (mediaCategories.Any(cat => cat.CategoryName.Equals(category.CategoryName, StringComparison.InvariantCultureIgnoreCase)))
+            return false;
+
+        // The expectations is that there is only a small number of categories and each will have only a small number of extensions. Hence, these iterative brute-force approach is ok.
+        foreach (var cat in mediaCategories)
+        {
+            foreach (var extension in cat.FileExtensions)
             {
-                SourceRoot = this.SourceRoot,
-                DestinationRoot = this.DestinationRoot,
-                RemoveSource = this.RemoveSource,
-                SkipExistingFiles = this.SkipExistingFiles,
-                DestinationPattern = this.DestinationPattern,
-                DeleteEmptyFolders = this.DeleteEmptyFolders,
-            };
-
-            result.MediaCategories.AddRange(this.MediaCategories ?? new List<MediaCategory>());
-
-            return result;
+                foreach (var newExtension in category.FileExtensions)
+                {
+                    if (extension.Equals(newExtension, StringComparison.InvariantCultureIgnoreCase))
+                    {
+                        return false; // Duplicate extension found
+                    }
+                }
+            }
         }
+
+        return true;
     }
 }
---
+++ src/MediaOrganizer/src/ViewModels/MediaCategoryEditorViewModel.cs
@@ -0,0 +1,116 @@
+﻿using MediaOrganizer.Core;
+using Prism.Commands;
+using Prism.Mvvm;
+using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Windows.Input;
+
+namespace MediaOrganizer.ViewModels;
+
+public class MediaCategoryEditorViewModel : BindableBase
+{
+    private readonly MediaCategory _mediaCategory;
+
+    private string _categoryName;
+    private string _categoryRoot;
+    private ObservableCollection<string> _extensions = new();
+
+    private readonly Action<MediaCategory> _onSave;
+    private readonly Action<MediaCategory> _onDelete;
+    private readonly Predicate<MediaCategory> _validator;
+
+    public string CategoryName
+    {
+        get => _categoryName;
+        set => SetProperty(ref _categoryName, value);
+    }
+
+    public string CategoryRoot
+    {
+        get => _categoryRoot;
+        set => SetProperty(ref _categoryRoot, value);
+    }
+
+    public ObservableCollection<string> Extensions
+    {
+        get => _extensions;
+        set
+        {
+            if (SetProperty(ref _extensions, value))
+                RaisePropertyChanged(nameof(ExtensionsText));
+        }
+    }
+
+    public string ExtensionsText
+    {
+        get => string.Join(", ", Extensions);
+        set
+        {
+            var cleaned = value?
+                .Split(',', StringSplitOptions.RemoveEmptyEntries)
+                .Select(e => e.Trim())
+                .Where(e => !string.IsNullOrWhiteSpace(e))
+                .Distinct()
+                .ToList() ?? new();
+
+            Extensions = new ObservableCollection<string>(cleaned);
+        }
+    }
+
+    public MediaCategoryEditorViewModel() : this(new MediaCategory(), cat => true, cat => { }, catToDelete => { })
+    {
+    }
+
+    public MediaCategoryEditorViewModel(MediaCategory category, Predicate<MediaCategory> validator, Action<MediaCategory> onSave, Action<MediaCategory> onDelete)
+    {
+        _mediaCategory = category ?? throw new ArgumentNullException(nameof(category));
+
+        _categoryName = category.CategoryName ?? string.Empty;
+        _extensions = category.FileExtensions == null ? new ObservableCollection<string>() : new ObservableCollection<string>(category.FileExtensions);
+
+        _onSave = onSave ?? throw new ArgumentNullException(nameof(onSave));
+        _onDelete = onDelete ?? throw new ArgumentNullException(nameof(onDelete));
+        _validator = validator ?? (_ => true);
+
+        SaveCommand = new DelegateCommand(Save, CanSave)
+            .ObservesProperty(() => CategoryName)
+            .ObservesProperty(() => ExtensionsText);
+
+        DeleteCommand = new DelegateCommand(Delete)
+            .ObservesCanExecute(() => ShowDeleteButton);
+    }
+
+    public bool ShowDeleteButton => !string.IsNullOrWhiteSpace(CategoryName);
+
+    public ICommand SaveCommand { get; }
+
+    public ICommand DeleteCommand { get; }
+
+    private void Save()
+    {
+        MediaCategory category = GetCategory();
+
+        if (_validator(category))
+        {
+            _onSave(category);
+        }
+    }
+
+    private void Delete()
+    {
+        _onDelete(this.GetCategory());
+    }
+
+    private bool CanSave()
+    {
+        return !string.IsNullOrWhiteSpace(CategoryName) && Extensions.Any();
+    }
+
+    private MediaCategory GetCategory() => new MediaCategory
+    {
+        CategoryName = CategoryName,
+        CategoryRoot = CategoryRoot,
+        FileExtensions = Extensions.ToArray()
+    };
+}
